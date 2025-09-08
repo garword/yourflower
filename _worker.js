fa518c055db25be7d0856355fa386841d6916ce6
garword <143305081+garword@users.noreply.github.com>
2025-09-08 09:26:41 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "xekeneli.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "bungamawara"; // Ganti dengan nama workers kalian
+const rootDomain = "pages.dev"; // Ganti dengan domain utama kalian
+const serviceName = "yourflower"; // Ganti dengan nama workers kalian
 const apiKey = "5Z9qBI5mUfEyZWk83Rji3JAgHSfgpJ8xytINiRkD"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = "xekeneli@cyclelove.cc"; // Ganti dengan email yang kalian gunakan
 const accountID = "fe4699cf79baa698c4eabcdbd2dac695"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
